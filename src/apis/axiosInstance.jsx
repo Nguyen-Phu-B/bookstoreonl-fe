3a5635b875b0877fb1f16eee955d14b8f6071204
Nguyen-Phu-B <nguyenhphub@gmail.com>
2024-05-09 16:52:49 +0700

    update

--- src/apis/axiosInstance.jsx
+++ src/apis/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "https://bookstoreonl-be.onrender.com/api/v1",
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "https://bookstoreonl-be.onrender.com/api/v1",
+    // baseURL: "http://localhost:3001/api/v1",
     timeout: 30000,
 });
 
